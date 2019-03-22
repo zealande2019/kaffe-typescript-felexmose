058f7991ca6f4e9d61bfee1b5abed04cd79d0b28
felex <felicxmose@msn.com>
2019-03-22 13:13:42 +0100

    added kaffe class

---
+++ src/js/Kaffe.ts
@@ -0,0 +1,16 @@
+abstract class Kaffe{
+    Rabat : number;
+
+    abstract Pris() : number;
+
+    Styrke() : string{
+        return "Mild";
+    }
+
+    constructor(rabat : number){
+        this.Rabat = rabat;
+    }
+
+
+
+}
